8bd1f2f3956be4fb93e22af369c84e42b63f97b3
pasqualeparillo <patparillo@gmail.com>
2019-07-01 10:21:34 -0400

    fixed localstorage issue for netlify test deploy

--- frontend/src/store/login.js
+++ frontend/src/store/login.js
@@ -1,9 +1,9 @@
 import React, { useState, createContext } from 'react'
 
 export const LoginContext = createContext();
-
+const windowGlobal = typeof window !== 'undefined' && window
 export function LoginProvider(props) {
-    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') || false);
+    const [isLoggedIn, setIsLoggedIn] = windowGlobal ? useState(windowGlobal.localStorage.getItem('isLoggedIn') || false) : useState();
     const [isLoginOpen, setLoginOpen] = useState(false);
     const [isRegisterOpen, setRegisterOpen] = useState(false);
     return (
