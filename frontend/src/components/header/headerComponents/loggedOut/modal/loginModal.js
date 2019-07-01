8457875c4639a9668638a0dacf24b7a238945ca1
pasqualeparillo <patparillo@gmail.com>
2019-07-01 11:57:57 -0400

    formatting

--- frontend/src/components/header/headerComponents/loggedOut/modal/loginModal.js
+++ frontend/src/components/header/headerComponents/loggedOut/modal/loginModal.js
@@ -20,10 +20,10 @@ export default function LoginModal(props) {
 
   return (
     <div
-      className="fixed flex w-full h-full bg-transparent shadow-lg top-0 left-0 right-0 bottom-0 bg-smoke-200 modal-translate"
+      className="fixed flex w-full h-full bg-transparent shadow-lg top-0 left-0 right-0 bottom-0 bg-smoke-200"
       ref={wrapperRef}
     >
-      <div className="flex relative flex-wrap modal-translate modal-size lg:w-1/2 w-5/6 bg-white shadow rounded-sm ">
+      <div className="flex relative flex-wrap  modal-size lg:w-1/2 w-5/6 bg-white shadow rounded-sm ">
         <HeroArea />
         <LoginForm />
       </div>
